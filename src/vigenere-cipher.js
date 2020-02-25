a6efc92426cc975e5b41daf443f1e695943c5829
Student-Java <energoslav@gmail.com>
2020-02-25 19:52:15 -0400

    feat: add vigenere chipher task solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,52 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+  constructor(directMachine) {
+    this.reverseMachine = (directMachine === false);
+    this.isLetter = /[a-z]/i;
+    this.mod = 26;
+    this.shift = 65;
+  }
+
+  encrypt(message, key) {
+    this.validateParameters(message, key);
+
+    return this.cryptoFunction(message, key, key => key.codePointAt(0));
+  }
+
+  decrypt(message, key) {
+    this.validateParameters(message, key);
+
+    return this.cryptoFunction(message, key, key => this.mod - key.codePointAt(0));
+  }
+
+  validateParameters(message, key) {
+    if (!(message || key)) {
+      throw Error();
+    }
+  }
+
+  getCypherString(length, key) {
+    return new Array(Math.ceil(length / key.length)).fill(key).join('');
+  }
+
+  cryptoFunction(message, key, cryptoAlgorithm) {
+    let result = [];
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let sym = message[i];
+      if (this.isLetter.test(sym)) {
+        let symCode = (sym.codePointAt(0) + cryptoAlgorithm(key[j++ % key.length])) % this.mod;
+        result.push(String.fromCharCode(symCode + this.shift));
+      } else {
+        result.push(sym);
+      }
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    return (this.reverseMachine ? result.reverse() : result).join('');
+  }
 }
 
 module.exports = VigenereCipheringMachine;
