4d5fd517bdeb8968dc38f2bfef63708dec38c713
Student-Java <energoslav@gmail.com>
2020-02-25 00:44:06 -0400

    feat: add transform array task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    let result = [];
+
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                result.pop();
+                break;
+            case '--double-next':
+                (i+1) < arr.length && result.push(arr[i + 1]);
+                break;
+            case '--double-prev':
+                (i - 1) > 0 && result.push(arr[i - 1]);
+                break
+            default:
+                result.push(arr[i]);
+        }
+    }
+
+    return result;
 };
