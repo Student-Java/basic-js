3da78c8c7eaa0222270fd0d198a980d755889a0e
Student-Java <energoslav@gmail.com>
2020-02-24 22:45:40 -0400

    feat: add carbon dating task solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
+const LN_2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+  let activity = parseFloat(sampleActivity);
+
+  return checkIfValid(activity, sampleActivity) && computeAge(activity);
 };
+
+let checkIfValid = (activity, initialValue) => !(isNaN(activity) || typeof initialValue !== 'string' || activity <= 0 || activity > MODERN_ACTIVITY);
+
+let computeAge = activity => Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (LN_2 / HALF_LIFE_PERIOD));
