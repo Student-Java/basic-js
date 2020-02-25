f73782c966126dac4fc28163423ac3cd5016d341
Student-Java <energoslav@gmail.com>
2020-02-25 14:30:23 -0400

    feat: add extended repeater task solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,10 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options, additionalSeparator) {
+    let additional;
+    let separator = typeof options.separator !== 'undefined' ? options.separator : '+';
+    additionalSeparator = typeof options.additionSeparator !== 'undefined' ? options.additionSeparator : '|';
+    if (typeof options.addition !== 'undefined') {
+        additional = repeater(options.addition, {repeatTimes: options.additionRepeatTimes, separator: additionalSeparator});
+    }
+
+    return Array(options.repeatTimes).fill(str + (additional ? additional : '')).join(separator);
 };
-  
